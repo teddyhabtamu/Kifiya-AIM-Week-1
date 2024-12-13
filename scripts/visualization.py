f206f6097f9fc24dd8c37cc624fb66e6b416d2e3
teddyhabtamu <tewodroshabtamu29@gmail.com>
2024-12-13 16:13:14 +0300

    Additional Modularization done

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_top_publishers(publisher_counts, top_n=10):
-    top_publishers = publisher_counts.head(top_n)
+def barchart(data, top_n=10, title='', xlabel='', ylabel=''):
+    top_publishers = data.head(top_n)
     top_publishers.plot(kind='bar', color='green', alpha=0.8)
-    plt.title('Top Publishers by Article Count')
-    plt.xlabel('Publisher')
-    plt.ylabel('Number of Articles')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.show()
 
-def plot_distribution(data, column, bins=50, color='blue', title='', xlabel='', ylabel=''):
-    plt.hist(data[column], bins=bins, color=color, alpha=0.7)
+def plot_distribution(column, bins=50, color='blue', title='', xlabel='', ylabel=''):
+    plt.hist(column, bins=bins, color=color, alpha=0.7)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
